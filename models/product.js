20f46051abb7d67c79a4c9bb723a9f8701cd91d0
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-02-03 15:39:28 -0300

    convertendo o models/product.js para prog funcional

--- models/product.js
+++ models/product.js
@@ -1,23 +1,8 @@
-'use strict';
-const {
-  Model
-} = require('sequelize');
-module.exports = (sequelize, DataTypes) => {
-  class Product extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
-    }
-  }
-  Product.init({
-    name: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Product',
+const Product = (sequelize, DataTypes) => {
+  const Product = sequelize.define('Product', {
+    name: DataTypes.STRING,
   });
   return Product;
 };
+
+module.exports = Product;
