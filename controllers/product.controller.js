c615b7ca52b1d6ef5f62dc16001e304d8292256c
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-02-03 21:28:10 -0300

    add other routes in product.controller.js

--- controllers/product.controller.js
+++ controllers/product.controller.js
@@ -15,3 +15,62 @@ router.post('/', (req, res) => {
       res.status(500).send({ message: 'Algo deu errado' });
     });
 });
+
+router.get('/', (req, res) => {
+  Product.findAll()
+    .then((products) => {
+      res.status(200).json(products);
+    })
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    });
+});
+
+router.get('/:id', (req, res) => {
+  Product.findByPk(req.params.id)
+    .then((product) => {
+      if (product === null) {
+        res.status(404).send({ message: 'Produto não encontrado' });
+      }
+      res.status(200).json(product);
+    })
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).json({ message: 'Algo deu errado' });
+    });
+});
+
+router.delete('/:id', (req, res) => {
+  Product.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+    .then((product) => {
+      res.status(200).send({ message: 'Produto excluído com sucesso' });
+    })
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).send({ message: 'Algo deu errado' });
+    });
+});
+
+router.put('/:id', (req, res) => {
+  const { name, description, price } = req.body;
+  Product.update(
+    { name, description, price },
+    {
+      where: {
+        id: req.params.id,
+      },
+    },
+  )
+    .then((product) => {
+      res.status(200).send({ message: 'Produto atualizado com sucesso' });
+    })
+    .catch((e) => {
+      console.log(e.message);
+      res.status(500).send({ message: 'Algo deu errado' });
+    });
+});
