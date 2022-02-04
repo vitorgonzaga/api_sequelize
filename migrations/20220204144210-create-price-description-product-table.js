6c573d09d00f0d734449e58c537774b3bd7cbe18
vitorgonzaga <vitor.gonzagaf1@gmail.com>
2022-02-04 14:55:29 -0300

    set migration as transaction

--- migrations/20220204144210-create-price-description-product-table.js
+++ migrations/20220204144210-create-price-description-product-table.js
@@ -1,23 +1,41 @@
 module.exports = {
-  up: async (queryInterface, Sequelize) => [
-    queryInterface.addColumn(
-      'Products',
-      'description',
-      {
-        type: Sequelize.STRING,
-      },
-    ),
-    queryInterface.addColumn(
-      'Products',
-      'price',
-      {
-        type: Sequelize.FLOAT,
-      },
-    ),
-  ],
+  up: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.addColumn(
+        'Products',
+        'description',
+        {
+          type: Sequelize.STRING,
+        },
+      );
+      await queryInterface.addColumn(
+        'Products',
+        'price',
+        {
+          type: Sequelize.FLOAT,
+        },
+      );
+      await transaction.commit();
+      return Promise.resolve();
+    } catch (error) {
+      if (transaction) await transaction.rollback();
+      return Promise.reject(error);
+    }
+  },
 
-  down: async (queryInterface, Sequelize) => [
-    queryInterface.removeColumn('Products', 'description'),
-    queryInterface.removeColumn('Products', 'price'),
-  ],
+  down: async (queryInterface, Sequelize) => {
+    const transaction = await queryInterface.sequelize.transaction();
+    try {
+      await queryInterface.removeColumn('Products', 'description', { transaction });
+      await queryInterface.removeColumn('Products', 'price', { transaction });
+      await transaction.commit();
+      return Promise.resolve();
+    } catch (error) {
+      if (transaction) {
+        await transaction.rollback();
+      }
+      return Promise.reject(error);
+    }
+  },
 };
